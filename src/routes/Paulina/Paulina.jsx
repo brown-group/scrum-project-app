01cf657e51e54f02881a3c1876bbfbe564c95eee
Paulina Kiendys <kiendyspaulina@gmail.com>
2024-04-30 14:03:52 +0200

    clean up

--- src/routes/Paulina/Paulina.jsx
+++ src/routes/Paulina/Paulina.jsx
@@ -1,32 +1,7 @@
-/* Feel free to use CSS or Bootstrap to style your HTML. Add functionality using JavaScript */
-
-import { useState } from "react";
-import "./Paulina.scss";
-
 export default function Paulina() {
-  const [title, setTitle] = useState("Paulina");
-
-  const myFunction = () => {
-    alert("Hello");
-  };
-
   return (
     <>
-      <h1>{title}</h1>
-
-      <button id="my-button" onClick={myFunction}>
-        CSS button
-      </button>
-
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={() => {
-          setTitle("Lorem ipsum");
-        }}
-      >
-        Bootstrap button
-      </button>
+      <h1>Paulina</h1>
     </>
   );
 }
