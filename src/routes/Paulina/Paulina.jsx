5f4b7b7e77c51f836826ccd8a5294a194e6c9770
Paulina Kiendys <kiendyspaulina@gmail.com>
2024-05-07 11:57:21 +0200

    add leaflet map component

--- src/routes/Paulina/Paulina.jsx
+++ src/routes/Paulina/Paulina.jsx
@@ -1,7 +1,22 @@
+import LeafletMap from "./components/LeafletMap";
+
 export default function Paulina() {
   return (
     <>
-      <h1>Paulina</h1>
+      <div className="container my-5">
+        <h1>Lorem ipsum</h1>
+        <div className="row">
+          <div className="col-12 col-md-6">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Perspiciatis nostrum iusto vel dolore quis! Animi error dolorem
+            laudantium esse, laboriosam sunt quas, alias, quam minima ducimus
+            commodi iusto facilis perferendis!
+          </div>
+          <div className="col-12 col-md-6">
+            <LeafletMap />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
