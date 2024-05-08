1691f7a237b431ea8d3cbd3c2e8e1db4baf85246
Paulina Kiendys <kiendyspaulina@gmail.com>
2024-05-08 10:34:36 +0200

    add marker

--- src/routes/Paulina/components/LeafletMap.jsx
+++ src/routes/Paulina/components/LeafletMap.jsx
@@ -1,17 +1,23 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function LeafletMap() {
+  const position = [51.505, -0.09];
   return (
     <MapContainer
-      center={[51.505, -0.09]}
+      center={position}
       zoom={13}
-      scrollWheelZoom={false}
+      scrollWheelZoom={true}
       style={{ height: "50vh" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }
